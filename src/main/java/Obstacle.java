2a7b961656ccb73a523a87f631620bb6ec7f4aae
Sonam Choudhary <sonachou2014@gmail.com>
2019-01-24 10:35:50 +0100

    adding obstacle

--- src/main/java/Obstacle.java
+++ src/main/java/Obstacle.java
@@ -1,2 +1,33 @@
 public class Obstacle {
+    private int x;
+    private int y;
+    private int oldX;
+    private int oldY;
+    private char symbolObstacle;
+
+
+    public Obstacle(int x, int y, char symbolObstacle) {
+        this.x = x;
+        this.y = y;
+        this.symbolObstacle = symbolObstacle;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getSymbolObstacle() {
+        return symbolObstacle;
+    }
+
+    public void scrollLeft(){
+        x--;
+
+    }
+
+
 }
